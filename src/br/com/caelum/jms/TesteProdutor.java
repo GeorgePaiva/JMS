2ec59c9841fdf25819f5c4a359fdecf017f669de
George Paiva <40047495+GeorgePaiva@users.noreply.github.com>
2020-11-30 15:40:05 -0300

    Teste balanceamento de carga com mais de um consumer

--- src/br/com/caelum/jms/TesteProdutor.java
+++ src/br/com/caelum/jms/TesteProdutor.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
-import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
 import javax.jms.Session;
-import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
-public class TesteConsumidor {
+public class TesteProdutor {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
@@ -32,23 +29,18 @@ public class TesteConsumidor {
 
 		// organizar o recebimento e a entrega das mensagens
 		Destination fila = (Destination) context.lookup("financeiro");
-		MessageConsumer consumer = session.createConsumer(fila);
+		
+		
+		MessageProducer producer = session.createProducer(fila);
+		
+		
+		for (int i = 0; i < 1000; i++) {
+			Message message = session.createTextMessage("<pedido><id>" + i + "</id></pedido>");
+			producer.send(message);
+		}
+		
 
-		consumer.setMessageListener(new MessageListener() {
-
-			@Override
-			public void onMessage(Message message) {
-				TextMessage textMessage = (TextMessage) message;
-				try {
-					System.out.println(textMessage.getText());
-				} catch (JMSException e) {
-					e.printStackTrace();
-				}
-			}
-
-		});
-
-		new Scanner(System.in).nextLine();
+		//new Scanner(System.in).nextLine();
 
 		session.close();
 		connection.close();
